c41bd97361311a6546e7eceeb6ac5e0c96c60fb4
363434879 <363434879@gotvdsb.ca>
2026-03-03 13:18:02 -0500

    Add project files.

---
+++ Topic 5- If Statements Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Topic_5__If_Statements_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Naika
+
+            Console.Title = "Topic 5 - If Statements Assignment";
+
+            Console.WriteLine("Welcome to the If Statements Assignment!");
+            Console.WriteLine();
+
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello " + name + "! Press ENTER to continue.");
+            Console.ResetColor();
+
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+
+    }
+
+}   
+
